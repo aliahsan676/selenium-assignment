6a05faf153a0e0c01adbe3c5a5beb1d9d62ca8d1
aliahsan676 <aliahsan676@gmail.com>
2022-10-19 01:09:53 +0600

    Selenium 6th Assignment

---
+++ src/test/java/selenium/assignment6/TestCaseTest.java
@@ -0,0 +1,40 @@
+package selenium.assignment6;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestCaseTest {
+
+    @Test
+    public void uploadFileShouldSucceed() throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        String imageRT = System.getProperty("user.dir") + "\\src\\test\\resources\\football.jpeg";
+        System.out.println(imageRT);
+
+        driver.findElement(By.id("file-upload")).sendKeys(imageRT);
+
+        Thread.sleep(3000);
+        driver.findElement(By.id("file-submit")).submit();
+        Thread.sleep(3000);
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("#content h3")).isDisplayed());
+
+        driver.quit();
+
+
+    }
+
+
+
+}
